3664287f49959a8fe2333fde2b7fc1900d81e47b
Krittin Khanueng <6110210013@psu.ac.th>
2021-04-30 21:31:33 +0700

    ep5

--- firestweb/myapp/urls.py
+++ firestweb/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import Home, About, Contact
+from .views import Home, About, Contact, AddProduct
 
 urlpatterns = [
 	path('', Home, name='home-page'),
 	path('about/', About, name='about-page'),
 	path('contact/', Contact, name='contact-page'),
+	path('addproduct/', AddProduct, name='addproduct-page')
 ]
--- firestweb/myapp/views.py
+++ firestweb/myapp/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Allproduct
 
 
 def Home(request):
-	product1 = 'แอปเปิ้ลเขียว'
-	product2 = 'องุ่น'
-	product3 = 'ทุเรียน'
-	context = {'product1': product1, 'product2': product2, 'product3': product3}
-	return render(request, 'myapp/home.html', context)
+    product1 = 'แอปเปิ้ลเขียว'
+    product2 = 'องุ่น'
+    product3 = 'ทุเรียน'
+    context = {'product1': product1, 'product2': product2, 'product3': product3}
+    return render(request, 'myapp/home.html', context)
 
 
 def About(request):
-	return render(request, 'myapp/about.html')
+    return render(request, 'myapp/about.html')
 
 
 def Contact(request):
-	return render(request, 'myapp/contact.html')
+    return render(request, 'myapp/contact.html')
+
+
+def AddProduct(request):
+    if request.method == 'POST':
+        data = request.POST.copy()
+        name = data.get('name')
+        price = data.get('price')
+        detail = data.get('detail')
+        imageurl = data.get('imageurl')
+
+        new = Allproduct()
+        new.name = name
+        new.price = price
+        new.detail = detail
+        new.imageurl = imageurl
+        new.save()
+
+    return render(request, 'myapp/addproduct.html')
