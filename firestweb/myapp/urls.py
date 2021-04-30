39f5754ffaef52df6e6ea461c37c8ec91f85749c
Krittin Khanueng <6110210013@psu.ac.th>
2021-05-01 00:21:11 +0700

    ep 6

--- firestweb/myapp/urls.py
+++ firestweb/myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import Home, About, Contact, AddProduct
+from .views import Home, About, Contact, AddProduct, Product
 
 urlpatterns = [
 	path('', Home, name='home-page'),
 	path('about/', About, name='about-page'),
 	path('contact/', Contact, name='contact-page'),
-	path('addproduct/', AddProduct, name='addproduct-page')
+	path('addproduct/', AddProduct, name='addproduct-page'),
+	path('allproduct/', Product, name='allproduct-page')
 ]
